ed50df8530a75327d96690f2b233d407421d99ac
Anisimov Stanislav <stanislav.anisimov.dev@gmail.com>
2023-03-01 21:23:00 +0300

    adding compositionAPI

--- src/stores/MovieStore.js
+++ src/stores/MovieStore.js
@@ -1,32 +1,33 @@
 import { defineStore } from "pinia";
-import { useMovieStore } from "./MovieStore";
-import { ref } from "vue";
+import { ref, computed } from "vue";
 
-const url =
-  "https:api.themoviedb.org/3/search/movie?api_key=02708c4929ad93aa5e68f8ee7bfa4445&query=";
-
-export const useSearchStore = defineStore("searchStore", () => {
-  const loader = ref(false);
+export const useMovieStore = defineStore("movieStore", () => {
   const movies = ref([]);
+  const activeTab = ref(2);
 
-  const getMovies = async (search) => {
-    loader.value = true;
-    const res = await fetch(`${url}${search}`);
-    const data = await res.json();
-    movies.value = data.results;
-    loader.value = false;
+  const watchedMovies = computed(() =>
+    movies.value.filter((el) => el.isWatched)
+  );
+  const totalCountMovies = computed(() => movies.value.length);
+
+  const setActiveTab = (id) => {
+    activeTab.value = id;
   };
-
-  const addToUserMovies = (object) => {
-    const movieStore = useMovieStore();
-    movieStore.movies.push({ ...object, isWatched: false });
-    movieStore.activeTab = 1;
+  const toggleWatched = (id) => {
+    const idx = movies.value.findIndex((el) => el.id === id);
+    movies.value[idx].isWatched = !movies.value[idx].isWatched;
+  };
+  const deleteMovie = (id) => {
+    movies.value = movies.value.filter((el) => el.id !== id);
   };
 
   return {
-    loader,
     movies,
-    getMovies,
-    addToUserMovies,
+    activeTab,
+    watchedMovies,
+    totalCountMovies,
+    toggleWatched,
+    deleteMovie,
+    setActiveTab,
   };
 });
