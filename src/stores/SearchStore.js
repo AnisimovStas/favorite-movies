4b3732ddc21f19f986627babc96576c654cd2f4a
Anisimov Stanislav <stanislav.anisimov.dev@gmail.com>
2023-03-01 21:56:21 +0300

    apikey fix

--- src/stores/SearchStore.js
+++ src/stores/SearchStore.js
@@ -2,28 +2,8 @@ import { defineStore } from "pinia";
 import { useMovieStore } from "./MovieStore";
 import { ref } from "vue";
 const url =
-  "https:api.themoviedb.org/3/search/movie?api_key=02708c4929ad93aa5e68f8ee7bfa4445&query=";
+  "https:api.themoviedb.org/3/search/movie?api_key=75b04b029189fe405cf16561bd9d8327&query=";
 
-// export const useSearchStore = defineStore("searchStore", {
-//   state: () => ({
-//     loader: false,
-//     movies: [],
-//   }),
-//   actions: {
-//     async getMovies(search) {
-//       this.loader = true;
-//       const res = await fetch(`${url}${search}`);
-//       const data = await res.json();
-//       this.movies = data.results;
-//       this.loader = false;
-//     },
-//     addToUserMovies(object) {
-//       const movieStore = useMovieStore();
-//       movieStore.movies.push({ ...object, isWatched: false }); //wow take a loo
-//       movieStore.activeTab = 1;
-//     },
-//   },
-// });
 export const useSearchStore = defineStore("searchStore", () => {
   const loader = ref(false);
   const movies = ref([]);
