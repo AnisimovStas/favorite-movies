70732713f046cc5fbacefcd37e0a30095d868ce9
Anisimov Stanislav <stanislav.anisimov.dev@gmail.com>
2023-03-01 20:04:51 +0300

    adding async actions

---
+++ src/stores/SearchStore.js
@@ -0,0 +1,19 @@
+import { defineStore } from "pinia";
+const url =
+  "https:api.themoviedb.org/3/search/movie?api_key=75b04b029189fe405cf16561bd9d8327&query=";
+
+export const useSearchStore = defineStore("searchStore", {
+  state: () => ({
+    loader: false,
+    movies: [],
+  }),
+  actions: {
+    async getMovies(search) {
+      this.loader = true;
+      const res = await fetch(`${url}${search}`);
+      const data = await res.json();
+      this.movies = data.results;
+      this.loader = false;
+    },
+  },
+});
